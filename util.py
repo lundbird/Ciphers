c0ebe68b4a2c7bb0e08915cfebbc268290e63af4
lundbird <alex.lundberg@gmail.com>
2018-11-21 10:14:01 -0600

    add number seive

--- util.py
+++ util.py
@@ -9,6 +9,43 @@ nums = {x:y for x,y in zip(alphabet,range(0,26))}
 letters = {y:x for x,y in zip(alphabet,range(0,26))}
 frequencies = collections.OrderedDict({'E':12.51,'T':9.25,'A':8.04,'O':7.60,'I':7.26,'N':7.09,'S':6.54,'R':6.12,'H':5.49,'L':4.14,'D':3.99,'C':3.06,'U':2.71,'M':2.53,'F':2.30,'P':2.00,'G':1.96,'W':1.92,'Y':1.73,'B':1.54,'V':0.99,'K':0.67,'X':0.19,'J':0.16,'Q':0.11,'Z':0.09})
 
+
+def Sieve(n): 
+      
+    # Create a boolean array "prime[0..n]" and initialize 
+    #  all entries it as true. A value in prime[i] will 
+    # finally be false if i is Not a prime, else true. 
+    prime = [True for i in range(n+1)] 
+    p = 2
+    while (p * p <= n): 
+        # If prime[p] is not changed, then it is a prime 
+        if (prime[p] == True): 
+              
+            # Update all multiples of p 
+            for i in range(p * 2, n+1, p): 
+                prime[i] = False
+        p += 1
+      
+    # collect all prime numbers 
+    primes = []
+    for p in range(2, n): 
+        if prime[p]: 
+            primes.append(p)
+    for i in range(len(primes)):
+        for j in range(i,len(primes)):
+            if primes[i]*primes[j]==n:
+                return primes[i],primes[j]
+
+
+def modExponentiate(x,power,mod):
+    val = x
+    for i in range(2,power+1):
+        val = val * x % mod
+    return val
+
+
+    
+            
 def modInverse(a, m) : 
     a = a % m
     for x in range(1, m):
@@ -75,10 +112,3 @@ if __name__=="__main__":
     print()
     print(estimate_N(ciphertext))
     print(find_N(ciphertext,20))
-
-    class Solution:
-    def twoSum(self, nums, target):
-        for i in range(nums):
-            for j in range(nums):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
